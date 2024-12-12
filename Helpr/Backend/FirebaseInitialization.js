00c9065dea8a92a9dc182ea1ebe0656f111a8d8f
GT <patnaikuni.gautam12@gmail.com>
2024-12-12 12:41:27 +0530

    Firebase connection v1.2

--- Helpr/Backend/FirebaseInitialization.js
+++ Helpr/Backend/FirebaseInitialization.js
@@ -1,13 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
-import firebase from "firebase/compat/app";
-import NewUserScreen from "../components/NewUserScreen";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCm6hx1yGUIV8A-Pm5IIVfK77rmO42XRe8",
   authDomain: "helpr-4f71a.firebaseapp.com",
@@ -20,4 +14,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth= getAuth(NewUserScreen)
+export const auth= getAuth(app)
